ab429b332e77ab715f805eb0cffe74d26aacfc36
SteinerTech <205841367+steinertech@users.noreply.github.com>
2025-06-28 23:41:31 +0200

    Deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,7 +6,7 @@ cd ../..
 
 cd pages
 git rm -r * # Delete all files and folders
-cp -r ../App/App.Web/dist/app.web/browser/ . # Copy
+cp -r ../App/App.Web/dist/app.web/browser/. . # Copy
 git config user.name "SteinerTech"
 git config user.email "205841367+steinertech@users.noreply.github.com" # https://github.com/settings/emails
 git add .
