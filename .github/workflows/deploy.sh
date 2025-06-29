60ce84d5643bf77ec978d0f1ba9d1e28a1ee80f3
SteinerTech <205841367+steinertech@users.noreply.github.com>
2025-06-29 14:14:59 +0200

    Deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 cd App/App.Web
 npm ci
@@ -9,9 +10,6 @@ cd ../..
 cd pages
 git rm -r * # Delete all files and folders
 cp -r ../App/App.Web/dist/app.web/browser/. . # Copy
-
-git config user.name "${{ github.actor }}"Add commentMore actions
-git config user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com"
 
 git add .
 git commit -m "Publish"
