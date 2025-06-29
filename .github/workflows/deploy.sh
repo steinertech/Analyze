fb89f5b72202857e93295b215b48116d7cba03e5
SteinerTech <205841367+steinertech@users.noreply.github.com>
2025-06-29 13:06:28 +0200

    Deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,7 +8,7 @@ cd pages
 git rm -r * # Delete all files and folders
 cp -r ../App/App.Web/dist/app.web/browser/. . # Copy
 git config user.name "SteinerTech"
-git config user.email "205841367+steinertech@users.noreply.github.com" # https://github.com/settings/emails
+git config user.email "${GITHUB_ACTOR_ID}+${GITHUB_ACTOR}@users.noreply.github.com" # https://github.com/settings/emails
 git add .
 git commit -m "Publish"
 git push
