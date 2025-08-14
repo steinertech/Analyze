03936de2cac52eaef416bb58d4ad70d23f38a0cd
SteinerTech <205841367+steinertech@users.noreply.github.com>
2025-08-14 12:15:16 +0200

    Deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,7 +8,9 @@ npm run buildLocalize
 cd ../..
 
 cd pages
+content=$(<CNAME) # Read CNAME file to preserve
 git rm -r * # Delete all files and folders
+echo "$content" > CNAME # Write CNAME file
 cp -r ../App/App.Web/dist/App.Web/browser/. . # Copy
 
 git add .
