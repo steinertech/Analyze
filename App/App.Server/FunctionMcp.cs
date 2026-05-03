61c106370e25096c423d20ec01fd79f43e42ee6f
User <205841367+steinertech@users.noreply.github.com>
2026-05-03 23:22:17 +0200

    Mcp for Version

--- App/App.Server/FunctionMcp.cs
+++ App/App.Server/FunctionMcp.cs
@@ -1,24 +1,21 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Mcp;
-using System.Text;
 
 public class FunctionMcp(Configuration configuration)
 {
-    [Function("app")]
-    public async Task<string> GetVersion(
-        [McpToolTrigger(toolName: "Version", description: "Gets current version of this app")] ToolInvocationContext context)
-        // [McpToolProperty(propertyName: "location", description: "The city name", isRequired: true)] string location)
+    [Function("version")]
+    public async Task<string> Version(
+        [McpToolTrigger(toolName: "Version", description: "Gets current version of this app")]
+        ToolInvocationContext toolContext)
     {
-        ArgumentException.ThrowIfNullOrEmpty(context.SessionId);
-        var aiSessionId = Convert.ToBase64String(Encoding.UTF8.GetBytes(context.SessionId));
-        var domain = configuration.TriggerDomainWithPort();
-        return $"{UtilServer.VersionServer} Session={context.SessionId};";
-        // return $"AUTH_REQUIRED: Please log in at https://{domain}/ai and enter this token {aiSessionId}";
-    }
-
-    [Function("app2")]
-    public string Description([McpToolTrigger(toolName: "Description", description: "Description of this MCP server")] ToolInvocationContext context)
-    {
-        return $"{UtilServer.VersionServer} Session={context.SessionId}; Server={configuration.TriggerDomainWithPort()}";
+        // See also npx @modelcontextprotocol/inspector
+        string? apiKey = null;
+        if (toolContext.TryGetHttpTransport(out var transport))
+        {
+            var headers = transport?.Headers;
+            headers?.TryGetValue("my-api-key", out apiKey);
+        }
+        // return $"AUTH_REQUIRED: Please log in at http://{configuration.TriggerDomainWithPort()}";
+        return $"{UtilServer.VersionServer} Session={toolContext.SessionId}; Domain={configuration.TriggerDomainWithPort()}; ApiKey={apiKey}";
     }
 }
