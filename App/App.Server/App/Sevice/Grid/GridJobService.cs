92b700a3524311a2f755258278d2ada7871e001e
User <205841367+steinertech@users.noreply.github.com>
2026-05-31 14:58:33 +0200

    Job status

--- App/App.Server/App/Sevice/Grid/GridJobService.cs
+++ App/App.Server/App/Sevice/Grid/GridJobService.cs
@@ -1,4 +1,4 @@
-﻿public class GridJobService(CommandContextService context, CosmosDbDynamicService cosmosDb) : GridServiceBase
+﻿public class GridJobService(CommandContextService context, CosmosDbService cosmosDb, CosmosDbDynamicService cosmosDbDynamic) : GridServiceBase
 {
     protected override Task<GridConfig> Config2(GridRequest2Dto request, GridEnum gridEnum)
     {
@@ -9,6 +9,7 @@
                 new() { FieldName = nameof(GridJobDto.Id), ColumnEnum = GridColumnEnum.Text, IsAllowModify = true },
                 new() { FieldName = nameof(GridJobDto.Name), ColumnEnum = GridColumnEnum.Text, IsAllowModify = true },
                 new() { FieldName = nameof(GridJobDto.Text), ColumnEnum = GridColumnEnum.Text, IsAllowModify = true },
+                new() { FieldName = nameof(GridJobDto.Status), ColumnEnum = GridColumnEnum.Text, IsAllowModify = true },
             ],
             IsAllowNew = true,
             FieldNameRowKey = nameof(GridJobDto.Id),
@@ -20,19 +21,57 @@
     protected override async Task<List<Dynamic>> GridLoad2(GridRequest2Dto request, string? fieldNameDistinct, GridConfig config, GridEnum gridEnum, string? modalName, GridLoadAutocomplete? autocomplete)
     {
         await context.UserAuthAsync();
-        var result = await cosmosDb.Select<GridJobDto>().ToListAsync();
+        var dataRowList = await cosmosDb.Select<GridJobDto>().ToListAsync();
+        var result = UtilGridReflection.DynamicFrom(dataRowList);
         result = await UtilGrid.GridLoad2(request, result, fieldNameDistinct, config, gridEnum);
+        foreach (var dataRow in result)
+        {
+            if (dataRow.TryGetValue(nameof(GridJobDto.Status), out var value))
+            {
+                if (value is GridJobStatus.Scheduled)
+                {
+                    dataRow.IconSet(nameof(GridJobDto.Status), "i-clock");
+                }
+                if (value is GridJobStatus.Running)
+                {
+                    dataRow.IconSet(nameof(GridJobDto.Status), "i-play");
+                }
+                if (value is GridJobStatus.Completed)
+                {
+                    dataRow.IconSet(nameof(GridJobDto.Status), "i-success");
+                }
+                if (value is GridJobStatus.Failed)
+                {
+                    dataRow.IconSet(nameof(GridJobDto.Status), "i-error");
+                }
+            }
+        }
         return result;
     }
 
     protected override async Task GridSave2(GridRequest2Dto request, List<Dynamic> sourceList, GridConfig config)
     {
         await context.UserAuthAsync();
-        await cosmosDb.UpsertAsync<GridJobDto>(sourceList, config);
+        await cosmosDbDynamic.UpsertAsync<GridJobDto>(sourceList, config);
     }
+}
+
+public enum GridJobStatus
+{
+    None = 0,
+
+    Scheduled = 1,
+
+    Running = 2,
+
+    Completed = 3,
+    
+    Failed = 4,
 }
 
 public class GridJobDto : DocumentDto
 {
     public string? Text { get; set; }
+
+    public GridJobStatus? Status { get; set; }
 }
